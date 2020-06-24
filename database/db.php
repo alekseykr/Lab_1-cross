bddee467d5b3f88e52ade2b434b2facf929f925a
alekseykr <61747957+alekseykr@users.noreply.github.com>
2020-06-24 11:09:34 +0300

    Добавление языка

--- database/db.php
+++ database/db.php
@@ -6,20 +6,23 @@ $users = [
         "password" => "123", 
         "role" => "2",
         "name" => "Вова", 
-        "surname" => "Менько"
+        "surname" => "Менько",
+        "lang"=>"ru"
     ],
     [
         "login" => "alex", 
         "password" => "1111",  
         "role" => "3",
         "name" => "Алексей", 
-        "surname" => "Кравченко"
+        "surname" => "Кравченко",
+        "lang"=>"en"
     ],
     [
         "login" => "serg",  
         "password" => "12345",  
         "role" => "1",
         "name" => "Сергей", 
-        "surname" => "Пупкин"
+        "surname" => "Пупкин",
+        "lang"=>"ua"
         ]
 ];
